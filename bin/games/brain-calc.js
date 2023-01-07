4d2b520555c90c5b4bff731ace1bc271aa3fb3c6
Timur Anvartdinov <t.anvartdinov@life-stream.tv>
2023-01-07 17:20:50 +0300

    add brain-calc

--- bin/games/brain-calc.js
+++ bin/games/brain-calc.js
@@ -1,5 +1,32 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
-import { greeting } from "../src/cli.js";
+import { game } from "../../src/index.js";
 
+const getQuestion = () => {
+  const getRandomInt = (min, max) => {
+    return Math.floor(min + Math.random() * (max - min));
+  };
+  const operator = getRandomInt(1, 4);
+  const firstOperand = getRandomInt(1, 100);
+  const secondOperand = getRandomInt(1, 100);
+
+  let rightAnswer = '';
+  let question = '';
+  switch (operator) {
+    case 1:
+      rightAnswer = `${firstOperand + secondOperand}`;
+      question = `${firstOperand} + ${secondOperand}`;
+      break;
+    case 2:
+      rightAnswer = `${firstOperand - secondOperand}`;
+      question = `${firstOperand} - ${secondOperand}`;
+      break;
+    case 3:
+      rightAnswer = `${firstOperand * secondOperand}`;
+      question = `${firstOperand} * ${secondOperand}`;
+      break;
+  }
+  return [question, rightAnswer];
+};
+
+game('What is the result of the expression?', getQuestion);
--- bin/games/brain-even.js
+++ bin/games/brain-even.js
@@ -12,28 +12,3 @@ const getQuestion = () => {
 };
 
 game('Answer "yes" if the number is even, otherwise answer "no".', getQuestion);
-
-// console.log('Welcome to the Brain Games!');
-// const name = greeting();
-//
-// console.log('Answer "yes" if the number is even, otherwise answer "no".')
-//
-// let countRightAnswers = 0;
-// while (true) {
-//   const secretNumber = getRandomInt(1, 100);
-//   console.log(`Question: ${secretNumber}`);
-//   const answer = readlineSync.question('Your answer: ');
-//   const rightAnswer = (secretNumber % 2 === 0) ? 'yes' : 'no';
-//   if (answer === rightAnswer) {
-//     countRightAnswers += 1;
-//     console.log('Correct!');
-//   } else {
-//     console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
-//     console.log(`Let's try again, ${name}!`);
-//     break;
-//   }
-//   if (countRightAnswers === 3) {
-//     console.log(`Congratulations, ${name}!`);
-//     break;
-//   }
-// }
