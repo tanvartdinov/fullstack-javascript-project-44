429d52348ed426aaedfeddf420827fe302ce0d05
Timur Anvartdinov <t.anvartdinov@life-stream.tv>
2023-01-07 14:22:42 +0300

    add brain-even

--- src/cli.js
+++ src/cli.js
@@ -3,6 +3,7 @@ import readlineSync from 'readline-sync';
 const greeting = () => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
+  return name;
 };
 
 export { greeting };
